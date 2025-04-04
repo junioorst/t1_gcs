4b5ae46d113591cb2ddfb4abc05f2afe23819a96
Júnior Stahl <juniorstahl@gmail.com>
2025-04-04 00:02:17 -0300

    menu admin incluidas opções restantes

--- src/App.java
+++ src/App.java
@@ -37,26 +37,26 @@ public class App {
                 System.out.print("Digite a opção desejada: ");
                 opcao = entrada.nextInt();
                 switch (opcao) {
-                    case 0:
+                    case 14:
                         break;
                     default:
                         System.out.println("Opcão inválida. Redigite.");
                 }
             }
-            while (opcao != 0);
+            while (opcao != 14);
         } else if (usuario.getTipo() == 1) {
             do {
-                operacoes();
+                menuFuncionario();
                 System.out.print("Digite a opção desejada: ");
                 opcao = entrada.nextInt();
                 switch (opcao) {
-                    case 0:
+                    case 5:
                         break;
                     default:
                         System.out.println("Opcão inválida. Redigite.");
                 }
             }
-            while (opcao != 0);
+            while (opcao != 5);
         }
     }
 
@@ -69,23 +69,32 @@ public class App {
         System.out.println();
     }
 
-    public void operacoes() {
-        System.out.println("[6] Registrar novo pedido de aquisição");
-        System.out.println("[7] Excluir pedido");
-        System.out.println("[8] Reabrir pedido");
-        System.out.println("[9] Trocar usuário");
-        System.out.println("[10] Sair\n");
+    public void menuFuncionario() {
+        System.out.println("\n==== PERMISSÕES GERAIS ====");
+        System.out.println("[1] Registrar novo pedido de aquisição");
+        System.out.println("[2] Excluir pedido");
+        System.out.println("[3] Reabrir pedido");
+        System.out.println("[4] Trocar usuário");
+        System.out.println("[5] Sair\n");
     }
 
     public void menuAdmin() {
-        System.out.println("\nPERMISSÕES ADMIN: ");
+        System.out.println("\n==== PERMISSÕES ADMIN ====");
         System.out.println("[1] Avaliar pedido aberto (aprovar/rejeitar)");
         System.out.println("[2] Listar todos os pedidos entre duas datas");
         System.out.println("[3] Buscar pedidos por funcionário solicitante");
         System.out.println("[4] Buscar pedidos pela descrição de um item");
         System.out.println("[5] Visualizar detalhes de um pedido");
+        System.out.println("[6] Número total de pedidos (aprovados/reprovados)");
+        System.out.println("[7] Número de pedidos dos últimos 30 dias e valor médio");
+        System.out.println("[8] Valor total de cada categoria nos últimos 30 dias");
+        System.out.println("[9] Pedido de maior valor ainda aberto");
 
-        System.out.println("\nPERMISSÕES GERAIS: ");
-        operacoes();
+        System.out.println("\n==== PERMISSÕES GERAIS ====");
+        System.out.println("[10] Registrar novo pedido de aquisição");
+        System.out.println("[11] Excluir pedido");
+        System.out.println("[12] Reabrir pedido");
+        System.out.println("[13] Trocar usuário");
+        System.out.println("[14] Sair\n");
     }
 }
